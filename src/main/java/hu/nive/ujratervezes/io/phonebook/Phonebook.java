0da24197a64ccb019dda04d2f2071306d4bfb4ab
oldcastle <32189785+oIdcastle@users.noreply.github.com>
2021-04-29 11:53:34 +0200

    Implement exportPhonebook() method

--- src/main/java/hu/nive/ujratervezes/io/phonebook/Phonebook.java
+++ src/main/java/hu/nive/ujratervezes/io/phonebook/Phonebook.java
@@ -1,3 +1,24 @@
 package hu.nive.ujratervezes.io.phonebook;
 
-public class Phonebook {}
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Map;
+
+public class Phonebook {
+
+    void exportPhonebook(Map<String, String> contacts, String output) {
+        if (contacts == null || output == null) {
+            throw new IllegalArgumentException("Invalid parameter");
+        }
+        try(BufferedWriter bw = new BufferedWriter(new FileWriter(output))) {
+            for (Map.Entry<String, String> e : contacts.entrySet()) {
+                bw.write(e.getKey() + ": " + e.getValue());
+                bw.newLine();
+            }
+        } catch (IOException e) {
+            System.out.println("I/O error: " + e.getMessage());
+        }
+    }
+
+}
